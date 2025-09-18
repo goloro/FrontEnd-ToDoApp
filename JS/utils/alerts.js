142188ba018a24e1c541994f30a37f73d1d9b239
Gonzalo <goloro02@gmail.com>
2025-09-18 19:19:47 +0200

    Completing functionality of create new project view

--- JS/utils/alerts.js
+++ JS/utils/alerts.js
@@ -2,13 +2,22 @@
 const alertBox = document.getElementById("alertBox")
 const alertBoxIcon = document.getElementById("alertBoxIcon")
 const alertBoxText = document.getElementById("alertBoxText")
+const alertBoxSubDiv = document.getElementById("alertBoxSubDiv")
 
 // ALERTS
 class AlertsClass {
-    constructor(icon, text, color) {
+    constructor(icon, text) {
         alertBox.style.display = "flex"
-        document.getElementById("alertBoxSubDiv").style.backgroundColor = color
-        alertBoxIcon.src = icon
+        if (icon === "success") {
+            alertBoxIcon.src = "https://api.iconify.design/bx/check-circle.svg?color=white"
+            alertBoxSubDiv.style.backgroundColor = "#67D980"
+        } else if (icon === "error") {
+            alertBoxIcon.src = "https://api.iconify.design/bx/error.svg?color=white"
+            alertBoxSubDiv.style.backgroundColor = "#E95E5E"
+        } else {
+            alertBoxIcon.src = "https://api.iconify.design/fluent/alert-12-regular.svg?color=white"
+            alertBoxSubDiv.style.backgroundColor = "#E95E5E"
+        }
         alertBoxText.innerHTML = text
 
         setTimeout(deleteAlert, 2000)
--- JS/utils/createProject.js
+++ JS/utils/createProject.js
@@ -1,4 +1,4 @@
-// CONSTANTS
+/*// CONSTANTS
 const backgroundInputColor = document.getElementById("selectedColorBackground")
 const textInputColor = document.getElementById("selectedColorText")
 const addColorBTN = document.getElementById("backgroundColorAdd")
@@ -30,4 +30,69 @@ addColorBTN.addEventListener("click", e => {
 // FUNCTIONS
 function checkDifferentColors() {
     if (backgroundInputColor.value != textInputColor.value) return true
+}*/
+
+// IMPORTS
+import { AlertsClass } from '../utils/alerts.js'
+
+// CONSTANTS
+// Buttons
+const createProjectBtn = document.getElementById("createNewProjectButton")
+
+// Inputs
+const titleInput = document.getElementById("newProjectTitleInput")
+const colorBackgroundInputPicker1 = document.getElementById("newProjectColorSelectorBackgroundInputPicker1")
+const colorBackgroundInputText1 = document.getElementById("newProjectColorSelectorBackgroundInputText1")
+const colorBackgroundInputPicker2 = document.getElementById("newProjectColorSelectorBackgroundInputPicker2")
+const colorBackgroundInputText2 = document.getElementById("newProjectColorSelectorBackgroundInputText2")
+const colorTextInputPicker = document.getElementById("newProjectColorSelectorTextInputPicker")
+const colorTextInputText = document.getElementById("newProjectColorSelectorTextInputText")
+
+// Preview
+const previewDiv = document.getElementById("newProjectPreview")
+const previewText = document.getElementById("newProjectPreviewText")
+
+// EVENT LISTENERS
+createProjectBtn.addEventListener("click", e => {
+    // TODO: check if any field is empty
+    // TODO: check if title is correct
+    // TODO: check if the color is a color hex code
+    if (!checkFieldsEmpty()) {}
+})
+colorBackgroundInputPicker1.addEventListener("input", e => {
+    // TODO: change the background of the preview and the value of the text 1
+})
+colorBackgroundInputText1.addEventListener("keydown", e => {
+    // TODO: change the background of the preview and the value of the color picker 1
+})
+colorBackgroundInputPicker2.addEventListener("input", e => {
+    // TODO: change the background of the preview and the value of the text 2
+})
+colorBackgroundInputText2.addEventListener("keydown", e => {
+    // TODO: change the background of the preview and the value of the color picker 2
+})
+colorTextInputPicker.addEventListener("input", e => {                                    
+    previewText.style.color = colorTextInputPicker.value
+    colorTextInputText.value = colorTextInputPicker.value
+})
+colorTextInputText.addEventListener("keydown", e => {
+    // TODO: check if its a real color hex code
+    if (e.key === "Enter") {
+        if (colorTextInputText.value != "") {
+            previewText.style.color = colorTextInputText.value
+            colorTextInputPicker.value = colorTextInputText.value
+        } else {
+            previewText.style.color = "#000000"
+            colorTextInputPicker.value = "#000000"
+        }
+    }
+})
+
+// FUNCTION
+function checkFieldsEmpty() {
+    if (titleInput.value == "") {
+        new AlertsClass("error", "The title field is empty")
+        return true
+    }
+    return false
 }
