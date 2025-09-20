384a7b49bdc4eceec55fe038f174b3499419e456
Gonzalo Lopez Rodriguez <goloro02@gmail.com>
2025-09-20 21:11:48 +0200

    Github-preview

---
+++ JS/loginGitHub.js
@@ -0,0 +1,8 @@
+// CONSTANTS
+// Buttons
+const loginBtn = document.getElementById("button-login")
+
+// EVENT LISTENERS
+loginBtn.addEventListener("click", e => {
+    window.open('../HTML/app.html', '_self')
+})
